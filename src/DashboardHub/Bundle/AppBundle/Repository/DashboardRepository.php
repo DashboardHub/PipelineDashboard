df21aa1a4a425107bc338e57ff7e9e5a21378462
Eddie Jaoude <eddie@jaoudestudios.com>
2015-03-08 17:38:12 +0000

    [#26] Dashboard Repository layer

---
+++ src/DashboardHub/Bundle/AppBundle/Repository/DashboardRepository.php
@@ -0,0 +1,55 @@
+<?php
+namespace DashboardHub\Bundle\AppBundle\Repository;
+
+use DashboardHub\Bundle\AppBundle\Entity\User;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class DashboardRepository
+ * @package DashboardHub\Bundle\AppBundle\Repository
+ */
+class DashboardRepository extends EntityRepository
+{
+
+    /**
+     * @param $username
+     *
+     * @return array
+     */
+    public function findAllByUserAndDefaults($username)
+    {
+        return $this->getEntityManager()
+                    ->createQuery(
+                        'SELECT d FROM DashboardHubAppBundle:Dashboard d
+                          JOIN d.user u
+                          WHERE
+                            u.username = :username
+                            OR
+                            u.username = :defaultUsername'
+                    )
+                    ->setParameter('username', $username)
+                    ->setParameter('defaultUsername', 'DashboardHub')
+                    ->getResult();
+    }
+
+    /**
+     * @param $id
+     *
+     * @return array
+     */
+    public function findOneByUsernameAndId($username, $id)
+    {
+        return $this->getEntityManager()
+                    ->createQuery(
+                        'SELECT d FROM DashboardHubAppBundle:Dashboard d
+                          JOIN d.user u
+                          WHERE
+                            u.username = :username
+                            AND
+                            d.id = :id'
+                    )
+                    ->setParameter('username', $username)
+                    ->setParameter('id', $id)
+                    ->getOneOrNullResult();
+    }
+}
