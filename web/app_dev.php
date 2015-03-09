e3e1a01bf1cd96e7fdf1a4b01a39d86ef19f9c5d
Eddie Jaoude <eddie@jaoudestudios.com>
2015-03-09 21:40:53 +0000

    [#71] Suppress ENV variable if doesn't exist

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,7 +10,7 @@ use Symfony\Component\Debug\Debug;
 // Local VMs will normally be rejected by this script as they are not considered development environments
 // This initial check allows a local setting of SYMFONY_ENV to force the allowance of the execution of
 // dev scripts in such cases.
-if ($_SERVER['SYMFONY_ENV'] != 'dev') {
+if (@$_SERVER['SYMFONY_ENV'] != 'dev') {
     // This check prevents access to debug front controllers that are deployed by accident to production servers.
     // Feel free to remove this, extend it, or make something more sophisticated.
     if (isset($_SERVER['HTTP_CLIENT_IP'])
