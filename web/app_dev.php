0669e11190a3ebe4e7fa7161d588f783b62051fb
Chris Moreton <chris@netsensia.com>
2015-03-09 17:30:39 +0000

    Update app_dev to use SYMFONY_ENV variable

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,7 +7,10 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-if (strtolower($_SERVER['HTTP_HOST']) != 'pipelinedashboard.local') {
+// This will bypass the non-dev check when SYMFONY_ENV is set explicity to DEV
+// This allows, for example, the server to run in dev mode in a virtual machine where 
+// otherwise access to this file would not be allowed.
+if ($_SERVER['SYMFONY_ENV'] != 'dev') {
     // This check prevents access to debug front controllers that are deployed by accident to production servers.
     // Feel free to remove this, extend it, or make something more sophisticated.
     if (isset($_SERVER['HTTP_CLIENT_IP'])
