9581696a4c5c4adf6ebfd9079872e01deb1ab9f6
Eddie Jaoude <eddie@jaoudestudios.com>
2015-03-12 13:14:50 +0000

    [#68] MockUser Entity added & used in Provider

--- src/DashboardHub/Bundle/DevelopmentBundle/Security/User/DashboardHubUserProvider.php
+++ src/DashboardHub/Bundle/DevelopmentBundle/Security/User/DashboardHubUserProvider.php
@@ -5,18 +5,18 @@ namespace DashboardHub\Bundle\DevelopmentBundle\Security\User;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use DashboardHub\Bundle\AppBundle\Entity\User;
+use DashboardHub\Bundle\DevelopmentBundle\Entity\MockUser;
 
 class DashboardHubUserProvider implements UserProviderInterface
 {
     public function loadUserByUsername($username)
     {
-        return new User('Mockerman');
+        return new MockUser('Mockerman');
     }
 
     public function refreshUser(UserInterface $user)
     {
-        if (!$user instanceof User) {
+        if (!$user instanceof MockUser) {
             throw new UnsupportedUserException(
                 sprintf('Instances of "%s" are not supported.', get_class($user))
             );
@@ -27,6 +27,6 @@ class DashboardHubUserProvider implements UserProviderInterface
 
     public function supportsClass($class)
     {
-        return $class === 'DashboardHub\Bundle\AppBundle\Entity\User';
+        return $class === 'DashboardHub\Bundle\DevelopmentBundle\Entity\MockUser';
     }
 }
