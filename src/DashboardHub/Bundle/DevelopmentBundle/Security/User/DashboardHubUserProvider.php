1af5e0e016df379b660c80f5dd65094555676f32
Eddie Jaoude <eddie@jaoudestudios.com>
2015-03-12 13:17:57 +0000

    [#68] Updated docbloc on User Provider

--- src/DashboardHub/Bundle/DevelopmentBundle/Security/User/DashboardHubUserProvider.php
+++ src/DashboardHub/Bundle/DevelopmentBundle/Security/User/DashboardHubUserProvider.php
@@ -7,13 +7,28 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use DashboardHub\Bundle\DevelopmentBundle\Entity\MockUser;
 
+/**
+ * Class DashboardHubUserProvider
+ * @package DashboardHub\Bundle\DevelopmentBundle\Security\User
+ */
 class DashboardHubUserProvider implements UserProviderInterface
 {
+
+    /**
+     * @param string $username
+     *
+     * @return MockUser
+     */
     public function loadUserByUsername($username)
     {
         return new MockUser('Mockerman');
     }
 
+    /**
+     * @param UserInterface $user
+     *
+     * @return MockUser
+     */
     public function refreshUser(UserInterface $user)
     {
         if (!$user instanceof MockUser) {
@@ -25,6 +40,11 @@ class DashboardHubUserProvider implements UserProviderInterface
         return $this->loadUserByUsername($user->getUsername());
     }
 
+    /**
+     * @param string $class
+     *
+     * @return bool
+     */
     public function supportsClass($class)
     {
         return $class === 'DashboardHub\Bundle\DevelopmentBundle\Entity\MockUser';
