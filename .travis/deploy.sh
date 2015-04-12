e6b75d84f90f84a21884bd36a2b7bf39db586ed9
Eddie Jaoude <eddie@jaoudestudios.com>
2015-04-12 19:42:51 +0100

    Moved all deploy code to deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+eval "$(ssh-agent -s)"
+chmod 600 .travis/deploy.pem
+ssh-add .travis/deploy.pem
+
 if [ "$1" = "release-0.2-pre-alpha" ]
 then
     git remote add ci dokku@alpha.dashboardhub.io:alpha
