b7009c5cacbafac6e3848d258e7904d06c60ac11
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-04-12 19:39:56 +0100

    Deploy script updated for release-0.2-pre-alpha branch

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "develop" ]
+if [ "$1" = "release-0.2-pre-alpha" ]
 then
-    git remote add ci dokku@alpha.dashboardhub.io:ci && git push ci $GIT_TAG:master -f
-    ssh dokku@alpha.dashboardhub.io run ci php app/console doctrine:schema:update --force
+    git remote add ci dokku@alpha.dashboardhub.io:alpha
+    git push alpha $GIT_TAG:master -f
+    ssh dokku@alpha.dashboardhub.io run alpha php app/console doctrine:schema:update --force
 fi
