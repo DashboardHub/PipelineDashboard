2c8154ba1dc01c0c599e43d8c872551a678cac50
Eddie Jaoude <eddie@jaoudestudios.com>
2015-04-13 07:46:34 +0100

    Debug code added to deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,7 +7,10 @@ ssh-add .travis/deploy.pem
 
 if [ "$1" = "release-0.2-pre-alpha" ]
 then
+    echo "add remote"
     git remote add ci dokku@alpha.dashboardhub.io:alpha
+    echo "pushing"
     git push alpha $GIT_TAG:master -f
+    echo "run migrations"
     ssh dokku@alpha.dashboardhub.io run alpha php app/console doctrine:schema:update --force
 fi
