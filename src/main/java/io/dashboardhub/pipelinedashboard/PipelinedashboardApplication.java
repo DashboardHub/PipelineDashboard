2bd8665ba2426b714c2ed79c470f37262d615cb9
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-13 18:28:54 +0000

    Application file

---
+++ src/main/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplication.java
@@ -0,0 +1,21 @@
+package io.dashboardhub.pipelinedashboard;
+
+import io.dashboardhub.pipelinedashboard.listener.AuthenticationListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+public class PipelinedashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinedashboardApplication.class, args);
+	}
+
+	@Bean
+	public AuthenticationListener authenticatonListener() {
+		return new AuthenticationListener();
+	}
+}
