02a21fae3b44b2c848b10a6ee2edd3c01cc0d492
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-22 18:07:16 +0000

    #188 Integration test server on port 8081

--- src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
@@ -2,6 +2,7 @@ package io.dashboardhub.pipelinedashboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PipelinedashboardApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:8081")
 public class PipelinedashboardApplicationTests {
 
 	@Test
