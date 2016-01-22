9f595233ccd6a1c14bcfdef0d75bc132b856824e
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-22 14:53:49 +0000

    #186 Guest accessing secure page redirects to the login page

--- src/test/java/io/dashboardhub/pipelinedashboard/LoginDefs.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/LoginDefs.java
@@ -3,30 +3,32 @@ package io.dashboardhub.pipelinedashboard;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginDefs {
 
+    private String baseUrl = "http://localhost:8081";
+
     private WebDriver driver = new FirefoxDriver();
 
     @Given("^I am not logged in$")
     public void I_am_not_logged_in() {
-        driver.get("http://localhost:8081/logout");
-    }
-
-    @Given("^I see (\\s+)$")
-    public boolean I_see(String text) {
-        return driver.getPageSource().contains(text);
+        this.driver.get(this.baseUrl + "/logout");
     }
 
     @When("^I try to access a secure page$")
     public void I_try_to_access_a_secure_page() {
-        System.out.format("Secure page");
+        this.driver.navigate().to(this.baseUrl + "/project");
     }
 
     @Then("^I get redirected to the login page$")
     public void I_get_redirected_to_the_login_page() {
-        System.out.format("Login page");
+        String currentUrl = this.driver.getCurrentUrl();
+
+        Assert.assertEquals(this.baseUrl + "/login", currentUrl);
+
+        this.driver.close();
     }
 }
--- src/test/resources/io/dashboardhub/pipelinedashboard/login.feature
+++ src/test/resources/io/dashboardhub/pipelinedashboard/login.feature
@@ -2,6 +2,5 @@ Feature: Application Security
 
   Scenario: Non Authorised user trying to access secure pages
     Given I am not logged in
-#      And I see "please login"
     When I try to access a secure page
     Then I get redirected to the login page
