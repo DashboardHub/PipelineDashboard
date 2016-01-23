49cc0b37ee7c79b56eafd45a9d24369794951725
Simon Casey <web.toasty@gmail.com>
2016-01-23 17:06:57 +0000

    #188 revert prev change and remove unneeded dependency

--- src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
@@ -3,16 +3,14 @@ package io.dashboardhub.pipelinedashboard;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationContextLoader;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PipelinedashboardApplication.class)
 @WebAppConfiguration
 @IntegrationTest("server.port:8081")
-@ContextConfiguration(classes = PipelinedashboardApplication.class, loader = SpringApplicationContextLoader.class)
 public class PipelinedashboardApplicationTests {
 
     @Test
