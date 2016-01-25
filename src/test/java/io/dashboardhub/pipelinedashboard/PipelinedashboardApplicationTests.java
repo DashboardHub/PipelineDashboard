ba257db6a4d103c9d2b27d825bdfa76040090ede
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-25 09:07:51 +0000

    #188 Changed from Selenium to MvcMock

--- src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
@@ -2,7 +2,6 @@ package io.dashboardhub.pipelinedashboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,7 +9,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PipelinedashboardApplication.class)
 @WebAppConfiguration
-@IntegrationTest("server.port:8081")
 public class PipelinedashboardApplicationTests {
 
     @Test
--- src/test/java/io/dashboardhub/pipelinedashboard/secure/GuestDefinitions.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/secure/GuestDefinitions.java
@@ -1,34 +1,62 @@
 package io.dashboardhub.pipelinedashboard.secure;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import io.dashboardhub.pipelinedashboard.PipelinedashboardApplication;
+import io.dashboardhub.pipelinedashboard.config.SecurityConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PipelinedashboardApplication.class})
+@WebIntegrationTest
 public class GuestDefinitions {
 
-    final private String baseUrl = "http://localhost:8081";
+    final private String baseUrl = "http://localhost";
 
-    private WebDriver driver = new FirefoxDriver();
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    private ResultActions result;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
 
     @Given("^I am not logged in$")
-    public void I_am_not_logged_in() {
-        this.driver.get(this.baseUrl + "/logout");
+    public void I_am_not_logged_in() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @When("^I try to access a secure page$")
-    public void I_try_to_access_a_secure_page() {
-        this.driver.get(this.baseUrl + "/project");
+    public void I_try_to_access_a_secure_page() throws Exception {
+        result = mockMvc.perform(MockMvcRequestBuilders.get("/project"));
     }
 
     @Then("^I get redirected to the login page$")
-    public void I_get_redirected_to_the_login_page() {
-        String currentUrl = this.driver.getCurrentUrl();
-
-        Assert.assertEquals(this.baseUrl + "/login", currentUrl);
-
-        this.driver.close();
+    public void I_get_redirected_to_the_login_page() throws Exception {
+        result.andExpect(MockMvcResultMatchers.redirectedUrl(baseUrl + "/login"));
     }
 }
