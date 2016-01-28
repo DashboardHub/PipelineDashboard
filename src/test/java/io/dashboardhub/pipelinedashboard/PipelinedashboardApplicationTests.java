6b0d4deda140637e7d174e8674574f8a1cdbdcde
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-28 08:14:04 +0000

    #197 Tests to run on a different port and use Test profile

--- src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
+++ src/test/java/io/dashboardhub/pipelinedashboard/PipelinedashboardApplicationTests.java
@@ -2,6 +2,8 @@ package io.dashboardhub.pipelinedashboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,6 +11,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PipelinedashboardApplication.class)
 @WebAppConfiguration
+@IntegrationTest("server.port:8082")
+@ActiveProfiles("test")
 public class PipelinedashboardApplicationTests {
 
     @Test
