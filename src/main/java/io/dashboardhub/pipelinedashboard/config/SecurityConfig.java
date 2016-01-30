21e056d6f973a7226555a5ff40b0adf1f4f3ec87
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-30 04:38:47 +0000

    #199 Local Font Awesome assets added to layout & security exception

--- src/main/java/io/dashboardhub/pipelinedashboard/config/SecurityConfig.java
+++ src/main/java/io/dashboardhub/pipelinedashboard/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
                 .authorizeRequests()
-                .antMatchers("/", "/login**", "/webjars/**").permitAll()
+                .antMatchers("/", "/login**", "/webjars/**", "/fonts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
