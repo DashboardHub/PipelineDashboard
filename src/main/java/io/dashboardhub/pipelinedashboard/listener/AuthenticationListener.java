83074585cd1e0fa96149fd36c75b9bbdbfa24753
Eddie Jaoude <eddie@jaoudestudios.com>
2016-01-31 08:09:40 +0100

    #185 Updated Authentication listener to Audit login

--- src/main/java/io/dashboardhub/pipelinedashboard/listener/AuthenticationListener.java
+++ src/main/java/io/dashboardhub/pipelinedashboard/listener/AuthenticationListener.java
@@ -1,6 +1,8 @@
 package io.dashboardhub.pipelinedashboard.listener;
 
+import io.dashboardhub.pipelinedashboard.domain.Login;
 import io.dashboardhub.pipelinedashboard.domain.User;
+import io.dashboardhub.pipelinedashboard.service.LoginService;
 import io.dashboardhub.pipelinedashboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -13,12 +15,21 @@ public class AuthenticationListener implements ApplicationListener<InteractiveAu
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private LoginService loginService;
+
     @Override
     public void onApplicationEvent(final InteractiveAuthenticationSuccessEvent event) {
         User user = this.userService.findByUsername(event.getAuthentication().getName());
 
         if (user == null) {
-            this.userService.save(new User(event.getAuthentication().getName()));
+            user = this.userService.save(new User(event.getAuthentication().getName()));
         }
+
+        Login login = new Login();
+        user = this.userService.findByUsername(event.getAuthentication().getName());
+        this.loginService.save(login);
+        user.getLogins().add(login);
+        this.userService.save(user);
     }
 }
