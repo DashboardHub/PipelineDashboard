5de541f56c371a55b50ed665d63442c1784f56ae
Eddie Jaoude <eddie@jaoudestudios.com>
2016-02-04 07:51:02 +0000

    #207 Action for Profile update (post)

--- src/main/java/io/dashboardhub/pipelinedashboard/controller/AccountController.java
+++ src/main/java/io/dashboardhub/pipelinedashboard/controller/AccountController.java
@@ -5,8 +5,12 @@ import io.dashboardhub.pipelinedashboard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AccountController {
@@ -16,9 +20,30 @@ public class AccountController {
 
     @RequestMapping(value = {"/profile"}, method = RequestMethod.GET)
     public String profile(Model model) {
-        User user = this.userService.findByCurrentUsername();
+        User user = this.userService.findByCurrentUser();
         model.addAttribute("user", user);
 
         return "account/profile";
+    }
+
+    @RequestMapping(value = {"/profile"}, method = RequestMethod.POST)
+    public String profileSave(
+            @Valid User user,
+            BindingResult bindingResult,
+            final RedirectAttributes redirectAttributes,
+            Model model
+    ) {
+
+        this.userService.findByCurrentUser();
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("error", "An error occurred please try again");
+
+            return "account/profile";
+        }
+
+        this.userService.saveByCurrentUser(user);
+        redirectAttributes.addFlashAttribute("success", "Successfully saved");
+
+        return "redirect:/profile";
     }
 }
