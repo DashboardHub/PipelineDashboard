ed1db5ee76cda909e2ebe3e4deff3278d592deae
Eddie Jaoude <eddie@jaoudestudios.com>
2016-02-06 14:44:05 +0000

    #174 Project Repository

--- src/main/java/io/dashboardhub/pipelinedashboard/repository/ProjectRepository.java
+++ src/main/java/io/dashboardhub/pipelinedashboard/repository/ProjectRepository.java
@@ -2,8 +2,11 @@ package io.dashboardhub.pipelinedashboard.repository;
 
 import io.dashboardhub.pipelinedashboard.domain.Project;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends PagingAndSortingRepository<Project, Long> {
+
+    Project findByUid(@Param("uid") String uid);
 }
