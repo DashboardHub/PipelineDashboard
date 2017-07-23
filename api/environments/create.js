ad89f9f0038e106de6cc66eef6359f7e69e87a47
Eddie Jaoude <eddie@jaoudestudios.com>
2017-07-23 06:27:13 +0100

    Validation to Environment fields added

--- api/environments/create.js
+++ api/environments/create.js
@@ -3,14 +3,44 @@
 const uuid = require('uuid');
 const dynamodb = require('../dynamodb');
 const config = require('../config');
+const validator = require('validator');
 
 module.exports.create = (event, context, callback) => {
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
-    if (typeof data.name !== 'string') {
-        console.error('Validation Failed');
-        callback(new Error('Couldn\'t create the environment item.'));
-        return;
+
+    if (typeof data.name !== 'string' || !validator.isLength(data.name, {min: 3, max: 32})) {
+        return callback(null, {
+            statusCode: 400,
+            body: {message: 'Validation Error: "name" is required and must be a "string" between 3 and 32'}
+        });
+    }
+
+    if (data.description) {
+        if (typeof data.description !== 'string' || !validator.isLength(data.description, {min: 3, max: 1024})) {
+            return callback(null, {
+                statusCode: 400,
+                body: {message: 'Validation Error: "description" is optional but a "string" must be between 3 and 1024'}
+            });
+        }
+    }
+
+    if (data.tags) {
+        if (!Array.isArray(data.tags)) {
+            return callback(null, {
+                statusCode: 400,
+                body: {message: 'Validation Error: "tags" is optional but must be an "array"'}
+            });
+        }
+    }
+
+    if (data.isPrivate) {
+        if (typeof data.isPrivate !== 'boolean') {
+            return callback(null, {
+                statusCode: 400,
+                body: {message: 'Validation Error: "isPrivate" is optional but must be a "boolean"'}
+            });
+        }
     }
 
     const params = {
