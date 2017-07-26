c2080eda65d00e2f7075773365ea648cf04cae58
Eddie Jaoude <eddie@jaoudestudios.com>
2017-07-26 07:45:21 +0100

    Moved to using Angular routing

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EnviromnentsComponent } from './enviromnents/enviromnents.component';
@@ -9,12 +10,21 @@ import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CovalentLayoutModule } from '@covalent/core';
 
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: EnviromnentsComponent},
+  { path: '**', redirectTo: '' }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     EnviromnentsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     CovalentLayoutModule,
