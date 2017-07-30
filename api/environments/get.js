8f51f8753ca3bcad7f9382fe4ed1df500c3bf90c
Eddie Jaoude <eddie@jaoudestudios.com>
2017-07-30 09:14:31 +0100

    Get item from API

---
+++ api/environments/get.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const dynamodb = require('../dynamodb');
+const config = require('../config');
+
+module.exports.get = (event, context, callback) => {
+    const params = {
+        TableName: config.dynamodb.environments.table,
+        Key: {
+            id: event.pathParameters.id,
+        }
+    };
+
+    // fetch all environments from the database
+    dynamodb.get(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            return callback(new Error('Couldn\'t fetch the item.'));
+        }
+
+        callback(null, {
+            headers: {
+                "Access-Control-Allow-Origin" : "*"
+            },
+            statusCode: 200,
+            body: JSON.stringify(result.Item)
+        });
+    });
+};
