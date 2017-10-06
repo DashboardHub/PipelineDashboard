9f739c70af2dbe526a6a921ca3eb399a954cdea7
Eddie Jaoude <eddie@jaoudestudios.com>
2017-10-06 10:40:17 +0100

    Auto confirm on cognito user creation

--- api/cognito/register.js
+++ api/cognito/register.js
@@ -1,12 +1,25 @@
 'use strict';
 
 module.exports.preSignUp = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
 
-    callback(null, {
-        headers: {
-            "Access-Control-Allow-Origin" : "*"
-        },
-        statusCode: 200,
-        body: JSON.stringify({ register: 'test' })
-    });
+    // TODO Confirm this is a user that should be auto-confirmed, e.g., has a valid token in event.request.validationData
+    let autoConfirm = true;
+
+    if (autoConfirm) {
+        event.response.autoConfirmUser = true;
+        console.log('Auto confirmed user');
+
+        if (event.request.userAttributes.hasOwnProperty("email")) {
+            event.response.autoVerifyEmail = true;
+            console.log('Set email verified');
+        }
+
+        if (event.request.userAttributes.hasOwnProperty("phone_number")) {
+            event.response.autoVerifyPhone = true;
+            console.log('Set phone_number verified');
+        }
+    }
+
+    callback(null, event);
 };
