40d7a1c89011a0d91204fc94ba1054cb579fb301
Eddie Jaoude <eddie@jaoudestudios.com>
2017-10-29 15:01:45 +0000

    Added prod environment variables

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
   api: 'https://iklni2x68e.execute-api.eu-west-2.amazonaws.com/dev',
-  version: 'x.x.x'
+  version: 'x.x.x',
+
+  auth: {
+    clientID: 'PEVMydSIuQhF4K2aLTQz36JEn2yczfDV',
+    domain: 'dashboardhub.eu.auth0.com',
+    callbackURL: 'http://pipelinedashboard-ui.s3-website.eu-west-2.amazonaws.com/callback'
+  }
 };
