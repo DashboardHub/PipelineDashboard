414a4d3be2686127e299ce565d88332afcc39b77
Eddie Jaoude <eddie@jaoudestudios.com>
2018-01-25 18:42:03 +0000

    #569 View the environment details

--- web/src/app/main/content/environment/environment-view/environment-view.component.ts
+++ web/src/app/main/content/environment/environment-view/environment-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fuseAnimations } from '../../../../core/animations';
 import { Environment } from "../environment";
+import { environment } from './../../../../../environments/environment';
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
@@ -11,6 +12,7 @@ import { ActivatedRoute } from "@angular/router";
 export class EnvironmentViewComponent implements OnInit {
 
   environment: Environment = new Environment('');
+  private url: string = environment.api;
 
   constructor(private route: ActivatedRoute) {
   }
@@ -19,4 +21,11 @@ export class EnvironmentViewComponent implements OnInit {
     this.environment = this.route.snapshot.data['environment'];
   }
 
+  getCommand(state: string) {
+    if (this.environment.tokens.length) {
+      return `curl -XPOST -H "Content-Type: application/json"  -d \'{ "release":"vX.Y.Z" }\' ${this.url}/environments/${this.environment.id}/deployed/${this.environment.tokens[0].id}/${state}`;
+    } else {
+      return 'Please create at least 1 token';
+    }
+  }
 }
