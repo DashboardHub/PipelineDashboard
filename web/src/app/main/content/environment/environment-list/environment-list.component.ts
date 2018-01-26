1e2d1ced9d765bcfe54527efdeba12deafdb800d
Eddie Jaoude <eddie@jaoudestudios.com>
2018-01-26 06:51:39 +0000

    List environments only click for your own environments

--- web/src/app/main/content/environment/environment-list/environment-list.component.ts
+++ web/src/app/main/content/environment/environment-list/environment-list.component.ts
@@ -3,6 +3,7 @@ import { fuseAnimations } from '../../../../core/animations';
 import { Environment } from "../environment";
 import { ActivatedRoute } from "@angular/router";
 import { List } from "../../list";
+import {Profile} from "../../auth/profile";
 
 @Component({
     selector   : 'environment-list',
@@ -13,12 +14,14 @@ import { List } from "../../list";
 export class EnvironmentListComponent implements OnInit {
 
   environments: List<Environment> = new List<Environment>();
+  profile: Profile;
 
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.environments = this.route.snapshot.data['environments'];
+    this.profile = this.route.snapshot.data['profile'];
   }
 
 }
