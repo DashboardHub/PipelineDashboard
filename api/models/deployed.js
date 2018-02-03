1be69675fc7a95720508545ad39045b24df9c127
Eddie Jaoude <eddie@jaoudestudios.com>
2018-02-03 14:22:41 +0100

    Increased the model read/write throughput

--- api/models/deployed.js
+++ api/models/deployed.js
@@ -38,7 +38,7 @@ const schema = new Schema({
         timestamps: true,
         useDocumentTypes: true,
         useNativeBooleans: true,
-        throughput: { read: 1, write: 1 }
+        throughput: { read: 5, write: 5 }
     });
 
 let model = client.dynamoose.model(config.dynamodb.deployed.table, schema, {
