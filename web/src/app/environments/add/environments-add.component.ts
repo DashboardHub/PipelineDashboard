109139a5119581b3553c612fd8d507bd6004eb8d
Eddie Jaoude <eddie@jaoudestudios.com>
2018-04-30 09:01:39 +0700

    feat(environment): #798 add environment

--- web/src/app/environments/add/environments-add.component.ts
+++ web/src/app/environments/add/environments-add.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { EnvironmentsAddForm } from "./environments-add.form";
+import { EnvironmentService } from "../environment.service";
+import { AbstractControl } from "@angular/forms";
+import { Environment } from "../environment.model";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material";
 
 @Component({
   selector: 'qs-environments-add',
   templateUrl: './environments-add.component.html',
 })
 export class EnvironmentsAddComponent {
+  public form: EnvironmentsAddForm = new EnvironmentsAddForm();
+
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private environmentService: EnvironmentService
+  ) {
+  }
+
+  submit(form: AbstractControl): void {
+    this.environmentService
+      .add(form.value)
+      .subscribe(
+        (environment: Environment) => this.router.navigate(['/environments', environment.id, 'view']),
+        (error) => this.snackBar.open(error.message, null, { duration: 5000 })
+      );
+  }
 
 }
