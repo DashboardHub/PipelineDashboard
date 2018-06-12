40cfce592389a70343d7eebdc577c9d6683e02fa
Eddie Jaoude <eddie@jaoudestudios.com>
2018-06-12 08:42:10 +0100

    feat(legal): #875 legal markdown added to relevant pages

--- web/src/app/legal/privacy/privacy.component.ts
+++ web/src/app/legal/privacy/privacy.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'qs-legal-privacy',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class PrivacyComponent {
 
+  public content: string;
+
+  constructor(private http: HttpClient) {
+    this.http.get(`/assets/legal/privacy.md`, { responseType: 'text' }).subscribe((content: string) => {
+      this.content = content;
+    });
+  }
 }
--- web/src/app/legal/terms-conditions/terms-conditions.component.ts
+++ web/src/app/legal/terms-conditions/terms-conditions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'qs-legal-terms-conditions',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class TermsConditionsComponent {
 
+  public content: string;
+
+  constructor(private http: HttpClient) {
+    this.http.get(`/assets/legal/terms-conditions.md`, { responseType: 'text' }).subscribe((content: string) => {
+      this.content = content;
+    });
+  }
 }
