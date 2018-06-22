3a2febc5dd3f91e8892b85de53bb009280cfe1ef
chowbertilla <chowbertilla@gmail.com>
2018-06-22 17:07:15 +0100

    feat(releases): #993 graph but stuck

--- web/src/app/environments/releases/environments-releases.component.ts
+++ web/src/app/environments/releases/environments-releases.component.ts
@@ -8,6 +8,7 @@ import { Token } from '../tokens/token.model';
 import { Environment } from '../environment.model';
 import { AbstractControl } from '@angular/forms';
 import { Deployed } from './deployed.model';
+import { TdDigitsPipe } from '@covalent/core/common';
 
 @Component({
   selector: 'qs-environments-releases',
@@ -18,11 +19,19 @@ export class EnvironmentsReleasesComponent implements OnInit {
   public environment: Environment;
   public form: ReleasesForm;
   public tokens: List<Token>;
+  public graph: { name: string, series: { value: number, name: string }[] }[];
 
   constructor(
     private route: ActivatedRoute,
     private releaseService: ReleaseService,
   ) {
+    this.graph = this.graph.map((group: any) => {
+      group.series = group.series.map((dataItem: any) => {
+        dataItem.name = new Date(dataItem.name);
+        return dataItem;
+      });
+      return group;
+    });
   }
 
   refresh(): void {
@@ -35,6 +44,7 @@ export class EnvironmentsReleasesComponent implements OnInit {
     this.environment = this.route.snapshot.data.environment;
     this.form = new ReleasesForm(this.route.snapshot.data.tokens, this.environment);
     this.tokens = this.route.snapshot.data.tokens;
+    this.graph = this.releases.list.map((release) => ({ name: release.version, series: [{ value: release.duration, name: release.latest.state }] }));
   }
 
   submit(form: AbstractControl): void {
@@ -44,5 +54,9 @@ export class EnvironmentsReleasesComponent implements OnInit {
     deployed.state = form.value.state;
     deployed.token = this.tokens.list.find((token: Token) => token.id === form.value.token);
     this.releaseService.add(deployed).subscribe(() => this.refresh());
+  }
+
+  axisDigits(val: any): any {
+    return new TdDigitsPipe().transform(val);
   }
 }
