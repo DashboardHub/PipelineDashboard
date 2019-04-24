f13c6d58f34159af896db8e3b6831d145f511473
Khushboo <web.khushboo@gmail.com>
2019-04-24 23:40:51 +0530

    chore(feat): #1192, Fixed private project should not access publicly

--- web/src/app/projects/edit/edit.component.ts
+++ web/src/app/projects/edit/edit.component.ts
@@ -2,23 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
-import { ProjectService } from '../../../services/project.service';
-import { catchError } from 'rxjs/operators';
-import { ProjectModel, RepositoryModel } from '../../../models/index.model';
 import { Subscription } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+// Dashboard hub models
+import { ProjectModel, RepositoryModel } from '../../../models/index.model';
+
+// Dashboard hub services
+import { AuthenticationService } from '../../../services/authentication.service';
+import { ProjectService } from '../../../services/project.service';
 
 @Component({
     selector: 'dashboard-projects-edit',
     templateUrl: './edit.component.html',
 })
 export class EditProjectComponent implements OnInit {
-
     private projectSubscription: Subscription;
     private saveSubscription: Subscription;
-    private repositorySubscription: Subscription;
     public uid: string;
     public projectForm: FormGroup;
     public repositories: RepositoryModel[] = [];
+    public project: ProjectModel = new ProjectModel();
 
     constructor(
         private route: ActivatedRoute,
@@ -26,6 +30,7 @@ export class EditProjectComponent implements OnInit {
         private form: FormBuilder,
         private snackBar: MatSnackBar,
         private projectService: ProjectService,
+        private authService: AuthenticationService
     ) {
         this.uid = this.route.snapshot.paramMap.get('uid');
     }
@@ -39,7 +44,14 @@ export class EditProjectComponent implements OnInit {
 
         this.projectSubscription = this.projectService
             .findOneById(this.uid)
-            .subscribe((project: ProjectModel) => this.projectForm.reset(project));
+            .subscribe((project: ProjectModel) => {
+                    this.project = project;
+                    this.projectForm.reset(project);
+                    // Added check for not authenticated user and private project details
+                    if ((project.type === 'private' && !this.authService.isAuthenticated) || !this.isAdmin()) {
+                        this.router.navigate(['/']);
+                    }
+            });
     }
 
     save(): void {
@@ -49,6 +61,10 @@ export class EditProjectComponent implements OnInit {
                 catchError((error: any): any => this.snackBar.open(error.message, undefined, { duration: 5000 }))
             )
             .subscribe(() => this.router.navigate(['/project', this.uid]));
+    }
+
+    isAdmin(): boolean {
+        return this.project.access.admin.includes(this.authService.profile.uid);
     }
 
     ngDestroy(): void {
