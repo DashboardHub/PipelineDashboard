cf1b988ad819c5c2e4cb0b2803173857a5235de4
Eddie Jaoude <eddie@jaoudestudios.com>
2019-05-12 09:03:37 +0100

    chore(refactor): #1214 imports in group to be alphabetical

--- web/src/app/pipes/user.pipe.ts
+++ web/src/app/pipes/user.pipe.ts
@@ -1,8 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { UserService } from '../core/services/user.service';
 import { UserModel } from '../shared/models/user.model';
+import { UserService } from '../core/services/user.service';
 
 @Pipe({ name: 'user' })
 export class UserPipe implements PipeTransform {
