de53453fe66ecd3facca13c7ebe99cabe8ac7e3e
Khushboo <web.khushboo@gmail.com>
2019-06-15 15:38:11 +0530

    feat(project): #1257 added contibutors data to repository dashboard

--- functions/src/mappers/github/user.mapper.ts
+++ functions/src/mappers/github/user.mapper.ts
@@ -7,11 +7,13 @@ import { GitHubEventModel } from './event.mapper';
 export interface GitHubUserInput {
   login: string;
   avatar_url: string;
+  url: string;
 }
 
 export interface GitHubUserModel {
   username: string;
   avatarUrl: string;
+  url: string;
 }
 
 export interface GitHubUserStatsModel {
@@ -34,6 +36,7 @@ export class GitHubUserMapper {
     return {
       username: input.login,
       avatarUrl: input.avatar_url,
+      url: input.url,
     };
   }
 }
