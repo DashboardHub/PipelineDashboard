b41adce2e73913d1a794fc4c93ec494ec27446ff
Khushboo <web.khushboo@gmail.com>
2019-06-19 00:42:58 +0530

    fix(sorting): #1316 sorting return type changed to void

--- web/src/app/core/services/sorting.service.ts
+++ web/src/app/core/services/sorting.service.ts
@@ -10,7 +10,7 @@ export class SortingService {
   /**
    * This function is used for sorting the list by date field
    */
-  sortList(list: any, field: any): any {
+  sortList(list: any, field: any): void {
     list.sort((obj1: any, obj2: any) => {
       const date1: Date = new Date(obj1[field]);
       const date2: Date = new Date(obj2[field]);
