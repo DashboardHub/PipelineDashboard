5e2f07f8c0458724fa247e80e480b84dc634f55d
Khushboo <web.khushboo@gmail.com>
2019-08-06 22:07:19 +0530

    fix(assets): assets path updated

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -17,15 +17,19 @@ export class AppComponent {
   ) {
     this._iconRegistry
       .addSvgIconInNamespace('assets', 'dashboardhub',
-        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/master/logo-horizontal-std.svg'));
+        this._domSanitizer.bypassSecurityTrustResourceUrl(
+          'https://raw.githubusercontent.com/DashboardHub/Assets/logo/master/logo-horizontal-std.svg'
+        ));
     this._iconRegistry
       .addSvgIconInNamespace('assets', 'dashboardhub_white',
-        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/master/logo-horizontal-white.svg'));
+        this._domSanitizer.bypassSecurityTrustResourceUrl(
+          'https://raw.githubusercontent.com/DashboardHub/Assets/logo/master/logo-horizontal-white.svg'
+        ));
     this._iconRegistry
       .addSvgIconInNamespace('assets', 'dashboardhub_icon',
-        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/master/icon-only-orange.svg'));
+        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/logo/master/icon-only-orange.svg'));
     this._iconRegistry
       .addSvgIconInNamespace('assets', 'github',
-        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/master//github.svg'));
+        this._domSanitizer.bypassSecurityTrustResourceUrl('https://raw.githubusercontent.com/DashboardHub/Assets/logo/master//github.svg'));
   }
 }
