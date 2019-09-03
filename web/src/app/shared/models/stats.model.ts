230068ab693993d07daed786d664cebe2d48bc64
Eddie Jaoude <eddie@jaoudestudios.com>
2019-09-03 08:30:04 +0100

    feat(stats): #1454 stats model interface

--- web/src/app/shared/models/stats.model.ts
+++ web/src/app/shared/models/stats.model.ts
@@ -1,6 +1,32 @@
-export class StatsModel {
+import { IModel, Model } from './model.model';
+
+export interface IStats extends IModel {
   projects: number;
   users: number;
   pings: number;
-  events?: number;
+  events: number;
+}
+
+export class StatsModel extends Model<IStats> implements IStats {
+  projects: number;
+  users: number;
+  pings: number;
+  events: number;
+
+  constructor(data?: IStats) {
+    super();
+    this.projects = data.projects ? data.projects : 0;
+    this.users = data.users ? data.users : 0;
+    this.pings = data.pings ? data.pings : 0;
+    this.events = data.events ? data.events : 0;
+  }
+
+  toData(): IStats {
+    return  {
+      projects: this.projects,
+      users: this.users,
+      pings: this.pings,
+      events: this.events,
+    };
+  }
 }
