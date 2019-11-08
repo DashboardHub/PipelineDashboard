611d538f4e333efebfde86d6b1d5b16ee378dba4
Khushboo <web.khushboo@gmail.com>
2019-11-08 22:28:12 +0530

    feat(automation): #1596 added automation scripts for homepage titles

--- web/test/step_definitions/browser.js
+++ web/test/step_definitions/browser.js
@@ -1,11 +1,16 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
-const url = 'http://localhost:8080';
+const url = 'http://localhost:4200';
 
 Given(/^the "([^"]*)" page is open$/, (path) => {
     cy.visit(`${url}/${path}`);
 });
 
 Then(/^the title on the page says "([^"]*)"$/, (check) => {
-    cy.title().should('include', check);
+  cy.title().should('include', check);
 });
+
+And(/^the text "([^"]*)" is in the element "([^"]*)"$/, (text, element) => {
+  cy.get(element).contains(text);
+});
+
