103c9b89bb6c88cdb09d823cb5e56e5ee1a2bc11
Eddie Jaoude <eddie@jaoudestudios.com>
2019-11-13 08:38:17 +0000

    feat(testing): #1601 environment type in config

--- web/test/plugins/index.js
+++ web/test/plugins/index.js
@@ -2,12 +2,13 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 const admin = require('firebase-admin');
 const serviceAccount = require('../../../firebase.enc.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-const db = admin.firestore();
-
 module.exports = (on, config) => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `https://pipelinedashboard-${config.TEST_ENV}.firebaseio.com`
+  });
+  const db = admin.firestore();
+
   on('file:preprocessor', cucumber());
 
   on('task', {
