311975826d58797ba37e9cc1a8e428b0820e46fd
Eddie Jaoude <eddie@jaoudestudios.com>
2019-12-01 15:20:02 +0000

    feat(test): #1611 public project list has resolve

---
+++ web/src/app/projects/list/list-my-projects.component.ts
@@ -0,0 +1,46 @@
+// Core components
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+// Application services/model
+import { ProjectService } from '@app/core/services/index.service';
+import { IProject } from '@shared/models/index.model';
+
+/**
+ * Repository rating component
+ */
+@Component({
+  selector: 'dashboard-list-my-projects',
+  templateUrl: './list-my-projects.component.html',
+})
+export class ListMyProjectsComponent implements OnInit, OnDestroy {
+
+  private projectSubscription: Subscription;
+
+  public projects: IProject[] = [];
+
+  /**
+   * Life cycle method
+   * @param repositoryService Repository service
+   * @param route ActivatedRoute
+   */
+  constructor(
+    private projectService: ProjectService
+  ) { }
+
+  /**
+   * Life cycle init method
+   */
+  ngOnInit(): void {
+    this.projectSubscription = this.projectService
+      .findMyProjects()
+      .subscribe((projects: IProject[]) => this.projects = projects);
+  }
+
+  /**
+   * Life cycle destroy method
+   */
+  ngOnDestroy(): void {
+    this.projectSubscription.unsubscribe();
+  }
+}
--- web/src/app/shared/components/private-public-project/private-public-project.component.ts
+++ web/src/app/shared/components/private-public-project/private-public-project.component.ts
@@ -1,11 +1,8 @@
 // Core modules
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, Input } from '@angular/core';
 
 // Dashboard hub model and services
-import { Router } from '@angular/router';
-import { ProjectService } from '@core/services/index.service';
-import { IProject, ProjectModel } from '../../models/index.model';
+import { IProject } from '../../models/index.model';
 
 /**
  * Private public project component
@@ -15,45 +12,10 @@ import { IProject, ProjectModel } from '../../models/index.model';
   templateUrl: './private-public-project.component.html',
   styleUrls: ['./private-public-project.component.scss'],
 })
-export class PrivatePublicProjectComponent implements OnInit, OnDestroy {
+export class PrivatePublicProjectComponent {
 
-  private projectSubscription: Subscription;
-  public projects: ProjectModel[] = [];
-  public showTitle: boolean;
-  @Input() title: string = 'My Projects';
+  @Input() public projects: IProject[] = [];
+  @Input() title: string;
+  @Input() showTitle: boolean = false;
 
-  /**
-   * Life cycle method
-   * @param projectService ProjectService
-   * @param router Router
-   */
-  constructor(
-    private projectService: ProjectService,
-    private router: Router
-  ) {
-  }
-
-  /**
-   * Life cycle init method
-   */
-  ngOnInit(): void {
-    if (this.router.url === '/') {
-      this.projectSubscription = this.projectService
-        .findPublicProjects()
-        .subscribe((projects: IProject[]) => this.projects = projects.map((project: IProject) => new ProjectModel(project)));
-    } else {
-      this.showTitle = true;
-      this.projectSubscription = this.projectService
-        .findMyProjects()
-        .subscribe((projects: IProject[]) => this.projects = projects.map((project: IProject) => new ProjectModel(project)));
-    }
-  }
-
-  /**
-   * Life cycle destroy method
-   */
-  ngOnDestroy(): void {
-    this.projectSubscription
-      .unsubscribe();
-  }
 }
