d3b894f372fdfa60666cedcc61ef80111a4067fd
Khushboo <web.khushboo@gmail.com>
2019-12-01 16:55:10 +0530

    fix(testing): #1618 formatted js file

--- web/test/support/index.js
+++ web/test/support/index.js
@@ -1,10 +1,10 @@
 import './commands';
 
 before(() => {
-  cy.task('db:delete', {collection: 'projects', subCollection: 'pings'})
-  cy.task('db:delete', {collection: 'repositories', subCollection: 'statuses'})
-  cy.task('db:delete', { collection:'repositories' });
-  cy.task('db:delete', { collection:'projects' })
-  cy.task('db:delete', { collection:'users' })
-  cy.task('db:delete', { collection:'userStats' })
+  cy.task('db:delete', { collection: 'projects', subCollection: 'pings'})
+  cy.task('db:delete', { collection: 'repositories', subCollection: 'statuses'})
+  cy.task('db:delete', { collection: 'repositories' });
+  cy.task('db:delete', { collection: 'projects' })
+  cy.task('db:delete', { collection: 'users' })
+  cy.task('db:delete', { collection: 'userStats' })
 });
