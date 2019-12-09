265d3852cfb61f3b63b3203037168a0d433b2df4
Khushboo <web.khushboo@gmail.com>
2019-12-09 23:01:18 +0530

    feat(testing): #1617 added test steps for finding row and then validate text

--- web/test/step_definitions/browser.js
+++ web/test/step_definitions/browser.js
@@ -1,9 +1,13 @@
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+import {
+  Given,
+  Then
+} from 'cypress-cucumber-preprocessor/steps';
 
 const url = Cypress.config('baseUrl');
+let row;
 
 Given(/^the "([^"]*)" page is open$/, (path) => {
-    cy.visit(`${url}/${path}`);
+  cy.visit(`${url}/${path}`);
 });
 
 Then(/^the title on the page says "([^"]*)"$/, (text) => {
@@ -12,6 +16,16 @@ Then(/^the title on the page says "([^"]*)"$/, (text) => {
 
 Then(/^the text "([^"]*)" is in the element "([^"]*)"$/, (text, element) => {
   cy.get(element).contains(text);
+});
+
+When(/^find the row of text "([^"]*)"$/, (text) => {
+  cy.contains('tr', text).then((elem) => {
+   row = elem;
+  })
+});
+
+Then(/^the text "([^"]*)" is in the row$/, (text) => {
+  cy.get(row).contains(text);
 });
 
 Then(/^the text "([^"]*)" is not in the element "([^"]*)"$/, (text, element) => {
